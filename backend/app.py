6c05bad9c0892d4e585bccdc2941dab28cec4065
kelly-shih <kshih@ualberta.ca>
2025-08-07 14:58:43 -0600

    test commit

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ CORS(app)
 
 @app.route("/api/data")
 def get_data():
-    return jsonify({"message": "commit test from Flask!", "data": [1, 2, 3]})
+    return jsonify({"message": "commit test 2 from Flask!", "data": [1, 2, 3]})
 
 
 def get_recent_commits(token=None, per_page=30):

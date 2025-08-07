3a25523db278eb0baf823ec2ef616825a19fbbde
kelly-shih <kshih@ualberta.ca>
2025-08-07 09:14:38 -0600

    commit test

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api/data')
 def get_data():
-    return jsonify({"message": "Testing from Flask!", "data": [1, 2, 3]})
+    return jsonify({"message": "Hello from Flask!", "data": [1, 2, 3]})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
